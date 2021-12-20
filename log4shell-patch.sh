cf7a393de185c3e7ee875b58755496b2c81f6c2d
Gabriel Titerlea <titergab@yahoo.com>
2021-12-20 20:14:12 +0200

    CF-1499: Update log4j to version 2.17

--- log4shell-patch.sh
+++ log4shell-patch.sh
@@ -5,105 +5,98 @@ if [ `id -u` != "0" ]; then
   exit 1
 fi
 
-if ! command -v zip &> /dev/null
-then
-  echo "'zip' is required to apply this patch. Please install it and try again."
-  exit 1
-fi
-
 set -e
 
-cd /fusion
-JNDI_LOOKUP_CLASS=org/apache/logging/log4j/core/lookup/JndiLookup.class
+# Move to the directory where this script is located.
+SCRIPT_DIR=$(dirname "$0")
+cd "${SCRIPT_DIR}"
+# Save the current directory for later use.
+SCRIPT_DIR=$(pwd)
 
-echo "-- Content Fusion Log4Shell patch --"
+LIB_DIR=${SCRIPT_DIR}/lib
+
+#
+# @param $1 image_name The name of the image where changes should be commited.
+# @param $2 container_id The ID of the docker container to patch.
+# @param $3 lib_path Path to the directory where jars are located inside the container.
+#
+function patch() {
+  local image_name=$1
+  local container_id=$2
+  local lib_path=$3
+
+  declare -a jars=("log4j-1.2-api*jar" "log4j-api*jar" "log4j-core*jar" "log4j-slf4j-impl*jar" "log4j-web*jar")
+
+  local patched_at_least_one_jar=false
+  echo "Patching ${image_name}"
+
+  for jar in "${jars[@]}"
+  do
+    echo -n "  ${jar}... "
+
+    local path_in_container
+    path_in_container=$(docker exec "${container_id}" find "${lib_path}" -name "${jar}")
+    path_in_container=$(echo "${path_in_container}" | tr -d '\r')
+
+    if [ "${path_in_container}" == "" ]; then
+      echo "Ok"
+    else
+      echo -n "Patching... "
+
+      docker exec "${container_id}" rm "${path_in_container}"
+      # shellcheck disable=SC2086
+      docker cp "${LIB_DIR}/"${jar} "${container_id}:${lib_path}"
+
+      echo "Ok"
+      patched_at_least_one_jar=true
+    fi
+  done
+
+  if [[ "${patched_at_least_one_jar}" == "true" ]]; then
+    echo -n "  Committing changes... "
+    docker commit "${container_id}" "${image_name}"
+  fi
+
+  echo "Done"
+  echo ""
+}
+
+echo "-- Content Fusion patch --"
 echo ""
 
-TMP_DIR=$(mktemp -d /tmp/log4shell.XXXXXX)
-echo "Created temp dir: ${TMP_DIR}"
-
-LS_IMAGE_NAME=$(docker images | grep license-servlet | awk '{print $1}')
-
-WA_VERSION=$(docker images | grep oxygenxml/webreviewer-webauthor | awk '{print $2}')
-CF_VERSION=$(docker images | grep oxygenxml/webreviewer-api | awk '{print $2}')
-LS_VERSION=$(docker images | grep "${LS_IMAGE_NAME}" | awk '{print $2}')
-
-echo "Detected webreviewer-webauthor version: ${WA_VERSION}"
-echo "Detected webreviewer-api version: ${CF_VERSION}"
-echo "Detected license-servlet version: ${LS_VERSION}"
+echo "Searching for containers to patch"
 echo ""
 
-WA_CONTAINER=$(docker-compose ps -q webauthor)
-WA_CONTAINER=$(echo "${WA_CONTAINER}" | tr -d '\r')
+WA_IMAGE_NAME=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep webreviewer-webauthor)
+CF_IMAGE_NAME=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep webreviewer-api)
+LS_IMAGE_NAME=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep license-servlet)
 
-CF_CONTAINER=$(docker-compose ps -q review-api)
-CF_CONTAINER=$(echo "${CF_CONTAINER}" | tr -d '\r')
+pushd /fusion 1> /dev/null
+  WA_CONTAINER=$(docker-compose ps -q webauthor)
+  WA_CONTAINER=$(echo "${WA_CONTAINER}" | tr -d '\r')
 
-LS_CONTAINER=$(docker-compose ps -q license-servlet)
-LS_CONTAINER=$(echo "${LS_CONTAINER}" | tr -d '\r')
+  CF_CONTAINER=$(docker-compose ps -q review-api)
+  CF_CONTAINER=$(echo "${CF_CONTAINER}" | tr -d '\r')
 
-echo "Found container for webauthor: ${WA_CONTAINER}"
-echo "Found container for review-api: ${CF_CONTAINER}"
+  LS_CONTAINER=$(docker-compose ps -q license-servlet)
+  LS_CONTAINER=$(echo "${LS_CONTAINER}" | tr -d '\r')
+popd 1> /dev/null
+
+echo "Found container for ${WA_IMAGE_NAME}: ${WA_CONTAINER}"
+echo "Found container for ${CF_IMAGE_NAME}: ${CF_CONTAINER}"
 echo "Found container for ${LS_IMAGE_NAME}: ${LS_CONTAINER}"
 echo ""
 
-
-echo "Searching for log4j-core in webauthor"
-WA_LOG4JCORE=$(docker-compose exec webauthor find /tomcat/webapps/oxygen-xml-web-author/WEB-INF/lib/ -name log4j-core*jar)
-WA_LOG4JCORE=$(echo "${WA_LOG4JCORE}" | tr -d '\r')
-
-if [ "${WA_LOG4JCORE}" == "" ]; then
-  echo "log4j-core not present in webauthor, nothing to patch."
-else
-  echo "Patching ${WA_LOG4JCORE}"
-  WA_JAR=${TMP_DIR}/webauthor.jar
-  docker cp "${WA_CONTAINER}:${WA_LOG4JCORE}" "${WA_JAR}"
-  zip -q -d "${WA_JAR}" "${JNDI_LOOKUP_CLASS}"
-  docker cp "${WA_JAR}" "${WA_CONTAINER}:${WA_LOG4JCORE}"
-  docker commit "${WA_CONTAINER}" "oxygenxml/webreviewer-webauthor:${WA_VERSION}"
-  echo "Done"
-fi
-echo ""
-
-
-echo "Searching for log4j-core in review-api"
-CF_LOG4JCORE=$(docker-compose exec review-api find /tomcat/webapps/ROOT/WEB-INF/lib/ -name log4j-core*jar)
-CF_LOG4JCORE=$(echo "${CF_LOG4JCORE}" | tr -d '\r')
-
-if [ "${CF_LOG4JCORE}" == "" ]; then
-  echo "log4j-core not present in review-api, nothing to patch."
-else
-  echo "Patching ${CF_LOG4JCORE}"
-  CF_JAR=${TMP_DIR}/review-api.jar
-  docker cp "${CF_CONTAINER}:${CF_LOG4JCORE}" "${CF_JAR}"
-  zip -q -d "${CF_JAR}" "${JNDI_LOOKUP_CLASS}"
-  docker cp "${CF_JAR}" "${CF_CONTAINER}:${CF_LOG4JCORE}"
-  docker commit "${CF_CONTAINER}" "oxygenxml/webreviewer-api:${CF_VERSION}"
-  echo "Done"
-fi
-echo ""
-
-
-echo "Searching for log4j-core in license-servlet"
-LS_LOG4JCORE=$(docker-compose exec license-servlet find /tomcat/webapps/ROOT/WEB-INF/lib/ -name log4j-core*jar)
-LS_LOG4JCORE=$(echo "${LS_LOG4JCORE}" | tr -d '\r')
-
-if [ "${LS_LOG4JCORE}" == "" ]; then
-  echo "log4j-core not present in license-servlet, nothing to patch."
-else
-  echo "Patching ${LS_LOG4JCORE}"
-  LS_JAR=${TMP_DIR}/license-servlet.jar
-  docker cp "${LS_CONTAINER}:${LS_LOG4JCORE}" "${LS_JAR}"
-  zip -q -d "${LS_JAR}" "${JNDI_LOOKUP_CLASS}"
-  docker cp "${LS_JAR}" "${LS_CONTAINER}:${LS_LOG4JCORE}"
-  docker commit "${LS_CONTAINER}" "${LS_IMAGE_NAME}:${LS_VERSION}"
-  echo "Done"
-fi
-echo ""
-
+patch "${WA_IMAGE_NAME}" "${WA_CONTAINER}" "/tomcat/webapps/oxygen-xml-web-author/WEB-INF/lib/"
+patch "${CF_IMAGE_NAME}" "${CF_CONTAINER}" "/tomcat/webapps/ROOT/WEB-INF/lib/"
+patch "${LS_IMAGE_NAME}" "${LS_CONTAINER}" "/tomcat/webapps/ROOT/WEB-INF/lib/"
 
 echo "Restarting Content Fusion"
-bash /fusion/admin/stop-content-fusion.sh
-bash /fusion/admin/start-content-fusion.sh
+echo ""
 
-echo "Log4Shell patch applied successfully"
+bash /fusion/admin/stop-content-fusion.sh
+echo ""
+bash /fusion/admin/start-content-fusion.sh
+echo ""
+
+echo "Patch applied successfully"
